6e552392ac82a35d5cc14b6aee31542125b373ba
RobFarthing_EPM <rob.farthing@epm.digital>
2023-03-23 14:51:35 +0000

    Clean up code

--- Needle/ARDemo/src/scripts/ArControls.ts
+++ Needle/ARDemo/src/scripts/ArControls.ts
@@ -46,25 +46,10 @@ export class ARControls extends Behaviour
     @serializable(GameObject)
     public _camera: THREE.Camera;;
 
-    /*
-    public _groundPlane: THREE.Plane = new THREE.Plane();
-    
-    //@ts-ignore
-    public _cameraPlane: THREE.Plane = new THREE.Plane();
-
-    public _hasGroundPlane: boolean = false;
-    //@ts-ignore
-    
-    public _groundOffset: THREE.Vector3 = new THREE.Vector3();
-    public _groundOffsetFactor: number = 0;
-    public _groundDistance: number = 0;
-    public _groundPlanePoint: THREE.Vector3 = new THREE.Vector3();
-*/
     public _raycaster = new THREE.Raycaster();
     public _raycaster1 = new THREE.Raycaster();
     public _cameraPlaneOffset = new THREE.Vector3();
     public _intersection = new THREE.Vector3();
-    //public _worldPosition = new THREE.Vector3();
     public _inverseMatrix = new THREE.Matrix4();
     private _rbs: Rigidbody[] = [];
 
@@ -91,27 +76,21 @@ export class ARControls extends Behaviour
     private ROTATION_RATE: number = 1.5;
     private goalYaw: number = 0;
 
-private _origin: THREE.Vector3 = new THREE.Vector3(0,0,0);
-private _direction: THREE.Vector3 = new THREE.Vector3();
+    private _origin: THREE.Vector3 = new THREE.Vector3(0,0,0);
+    private _direction: THREE.Vector3 = new THREE.Vector3();
 
-private isRotateScale: boolean = false;
+    private isRotateScale: boolean = false;
 
 public screenPointToRay(x: number, y: number, ray?: THREE.Ray): THREE.Ray {
     const origin = new THREE.Vector3(x,y,-1);
-    //console.log(origin);
     this.convertScreenspaceToRaycastSpace(origin);
-    //this.context.input.convertScreenspaceToRaycastSpace(origin);
     origin.unproject(this.context.mainCamera);
-    //console.log(origin);
     const dir = this._direction.set(origin.x, origin.y, origin.z);
-    //console.log(dir);
     const camPosition = getWorldPosition(this.context.mainCamera);
-    //console.log(camPosition);
     dir.sub(camPosition);
     dir.normalize();
     if (ray) {
         ray.set(camPosition, dir);
-        //console.log("ray", ray);
         return ray;
     }
     else {
@@ -146,13 +125,6 @@ convertScreenspaceToRaycastSpace(vec2: Vec2) {
     }
 }
 
-/*
-onXRStarted()
-{
-
-}
-*/
-
 *WaitThenReset() {
     while (true) {
         yield WaitForSeconds(0.5);
@@ -163,10 +135,7 @@ onXRStarted()
 
 start()
 {
-   // WebXR.addEventListener(WebXREvent.XRStarted, this.onXRStarted.bind(this));
-    
    window.addEventListener("pointerdown", e => {
-    //console.log("pointerdown", e);
     if (!this.context.isInAR) {
         console.log("Wasn't in AR");
         this.isDragging = true;
@@ -176,7 +145,6 @@ start()
     this.touched();
     if(e != null)
     {
-        
     }
 }
 });
@@ -187,7 +155,6 @@ start()
         if (!this.context.isInAR) {
             if(this.isDragging == true)
             {
-                //console.log("pointermove");
         this.activePointerEvent = e;
         GameObject.setActive(this.object2, true);
             }
@@ -198,7 +165,6 @@ start()
 
     //@ts-ignore
     window.addEventListener("pointerup", e => {
-        //console.log("pointerup");
         if (!this.context.isInAR) {
         this.isDragging = false;
         GameObject.setActive(this.object1, false);
@@ -214,8 +180,6 @@ start()
 
 
     window.addEventListener("touchstart", e => {
-        //console.log("touchstart", e);
-        //console.log(e.targetTouches);
         this.activeTouchEvent = e;
         this.initialScale = this.gameObject.scale.x;
         this.initialWorldAngle = utils.getWorldRotation(this.gameObject);
@@ -236,36 +200,22 @@ start()
         this.startCoroutine(this.showThenHideTouchCube(), FrameEvent.Update);
         GameObject.setActive(this.object2, false);
         GameObject.setActive(this.object3, false);
-      
-        
     });
    
     //@ts-ignore
     window.addEventListener("touchmove", e => {
-        //console.log("touchmove");
         this.activeTouchEvent = e;
         this.isDragging = true;
         GameObject.setActive(this.object2, true);
-        /*
-        if(e.touches.length >= 2)
-        {
-            var a = e.touches[0].clientX - e.touches[1].clientX;
-            //this.gameObject.rotation.y += a * 0.0002;
-        }
-        */
-
     });
 
     //@ts-ignore
     window.addEventListener("touchend", e => {
-        //console.log("touchend");
-        //console.log(e.touches.length);
         if(e.touches.length == 0)
         {
             this.stopCoroutine(this.WaitThenReset());
             this.startCoroutine(this.WaitThenReset());
         }
-        //this.isTouch = false;
         this.isDragging = false;
         GameObject.setActive(this.object1, false);
         GameObject.setActive(this.object2, false);
@@ -296,13 +246,11 @@ start()
 
 public touched()
 {
- //console.log("touched");
  this.setTargetFromRaycast();
 }
 
 public unTouched()
 {
- //console.log("unTouched");
  this.clearTarget();
 }
 
@@ -357,15 +305,10 @@ private setTargetFromRaycast() {
     else
     {
     const rc = this.context.physics.raycast();
-    //console.log(rc);
-
     //@ts-ignore
     this._raycaster.setFromCamera(rc, this.context.mainCamera);
-    //Gizmos.DrawRay(this._raycaster.ray.origin, this._raycaster.ray.direction, 0xff0000, 10);
     for (const hit of rc) {
-        //console.log("Set target hit: ", hit.object.name);
         if (hit.distance > 0 && GameObject.isActiveInHierarchy(hit.object)) {
-            //console.log("Set target", hit.object.name, hit.object);
             //@ts-ignore
             this.mainTarget = hit.object as THREE.Object3D;
             if(hit.object.name == "GroundPlane")
@@ -392,7 +335,6 @@ private setTargetFromRaycast() {
 
 private clearTarget()
 {
-    
     if(this.arctrl != null)
     {
     this.isSelected = false;
@@ -400,8 +342,6 @@ private clearTarget()
     }
     this.mainTarget = null;
     this.arctrl = null;
-    //Keep orbit script disabled for now
-    //if (this.orbit) this.orbit.enabled = true;
 }
 
 update()
@@ -428,18 +368,11 @@ update()
                         {
                         const touch = this.activeTouchEvent.touches[0];
                         const ray = this.screenPointToRay(touch.clientX, touch.clientY);
-                        //const ray = this.context.mainCameraComponent!.screenPointToRay(touch.clientX, touch.clientY);
-                        //Gizmos.DrawRay(ray.origin, ray.direction, 0xff0000, 10);
-            
-                        //const rc = new THREE.Vector2(touch.clientX, touch.clientY);
                         this._raycaster.ray = ray;
-                        //Gizmos.DrawRay(this._raycaster.ray.origin, this._raycaster.ray.direction, 0xff0000, 10,);
-                    
                         const opts = new RaycastOptions();
                         const hits = this.context.physics.raycastFromRay(ray, opts);
                         for (let j = 0; j < hits.length; j++) {
                                 const hit = hits[j];
-                                //console.log(hit.object.name)
                                 if(hit.object.name == "GroundPlane")
                                 {
                                     setWorldPosition(this.gameObject, hit.point);
@@ -456,102 +389,9 @@ update()
                         var b = this.activeTouchEvent.touches[0].clientY - this.activeTouchEvent.touches[1].clientY;
                         var dist = Mathf.clamp((Math.sqrt(a*a + b*b) / 350), 0, 10);
                         var factor = dist / this.initialDist;
-                        //console.log("dist: ", dist);
                         var mainScale = Mathf.clamp(this.initialScale * (factor), 0.1, 10);
-                        //console.log(factor);
                         this.gameObject.scale.set(mainScale, mainScale, mainScale);
-                        //this.gameObject.scale.set(dist, dist, dist);
-
-                        //Raycast touch
-                        /*
-                        const touch1 = this.activeTouchEvent.touches[1];
-                        const ray = this.screenPointToRay(touch1.clientX, touch1.clientY);
-                        //const ray = this.context.mainCameraComponent!.screenPointToRay(touch.clientX, touch.clientY);
-                        //Gizmos.DrawRay(ray.origin, ray.direction, 0xff0000, 10);
-            
-                        //const rc = new THREE.Vector2(touch1.clientX, touch1.clientY);
-                        this._raycaster.ray = ray;
-                        //Gizmos.DrawRay(this._raycaster.ray.origin, this._raycaster.ray.direction, 0xff0000, 10,);
-                    
-                        const opts = new RaycastOptions();
-                        const hits = this.context.physics.raycastFromRay(ray, opts);
-                        
-                        if(hits.length > 0)
-                        {
-                            var hitPoint = hits[0].point;
-                        }
-                        */
-                        /*
-                        for (let j = 0; j < hits.length; j++) {
-                                const hit = hits[j];
-                                if(hit.object.name == "GroundPlane")
-                                {
-                                  
-                                }
-                                
-                            }  
-                            */       
-                           /*
-                        const fingerOne = new THREE.Vector2(this.activeTouchEvent.touches[0].clientX, this.activeTouchEvent.touches[0].clientY);
-                        console.log(fingerOne);
-                        const fingerTwo = new THREE.Vector2(this.activeTouchEvent.touches[1].clientX, this.activeTouchEvent.touches[1].clientY);
-                        console.log(fingerTwo);
-                        const deltaX = fingerTwo.x - fingerOne.x;
-                        const deltaY = fingerTwo.y - fingerOne.y;
-                        console.log(deltaX, deltaY);
-                        //Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-                        const angle = Math.atan2(deltaY, deltaX);
-                        console.log(angle);
-                        let deltaYaw = this.lastAngle - angle;
-                        if (deltaYaw > Math.PI) {
-                        deltaYaw -= 2 * Math.PI;
-                        } 
-                        else if (deltaYaw < -Math.PI) {
-                        deltaYaw += 2 * Math.PI;
-                        }
-                        console.log(deltaYaw);
-                        this.lastAngle = angle;
-                        //separation = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-                        const newangle = this.activeTouchEvent.touches[1].clientX * this.ROTATION_RATE;
-                        this.goalYaw += newangle - this.lastAngle;
-                        this.lastAngle = newangle;
-                        console.log(this.goalYaw);
-                        this.gameObject.rotation(new THREE.Vector3(0,1,0), this.goalYaw);
-                        */
-                        
-                        //current
-                        /*
-                        if(hitPoint != null)
-                        {
-                            var hitpointscreen = new THREE.Vector3(a, 0, 0);
-                        var mx = new THREE.Matrix4().lookAt(this.gameObject.position,hitpointscreen,new THREE.Vector3(0,1,0));
-                        var qt = new THREE.Quaternion().setFromRotationMatrix(mx);
-                        var tt = new THREE.Quaternion();
-                        qt = tt.multiplyQuaternions(qt,this.gameObject.quaternion);
-                        console.log(mx);
-                        console.log(qt);
-                        var diffAngle = this.initialWorldAngle.angleTo(hitPoint);
-                        //console.log(hitPoint);
-                        //console.log("angle: ", diffAngle);
-                        qt.x = 0;
-                        qt.z = 0;
-                        this.gameObject.quaternion.slerp(qt, this.context.time.deltaTime);
-                        //Rotate
-                        //this.gameObject.rotateOnWorldAxis(new THREE.Vector3( 0,1,0), -(a * 0.0005));
-                        if(this.initialRotPos > a)
-                        {
-                            //this.gameObject.rotateOnWorldAxis(new THREE.Vector3( 0,1,0), diffAngle);
-                            
-                        }
-                        else if (this.initialRotPos < a)
-                        {
-                            //this.gameObject.rotateOnWorldAxis(new THREE.Vector3( 0,1,0), -diffAngle);
-                        }
-                    }
-                    */
-
-                    //this.gameObject.rotation.y += a * 0.005;
-                    this.gameObject.rotation.y += a * 0.0002;
+                        this.gameObject.rotation.y += a * 0.0002;
                         break;
 
                 }
@@ -560,30 +400,22 @@ update()
     //Otherwise is a pointer
         else
         {
-        console.log("jeff");
-
             GameObject.setActive(this.pointerObject, true);
             for (let i = 0; i < this.activePointerEvent.pointerType.length; i++) {
                 const rc = this.context.input.getPointerPositionRC(0);
                 this._raycaster.setFromCamera(rc, this.context.mainCamera);
                 const ray = this._raycaster.ray;
-                //Gizmos.DrawRay(this._raycaster.ray.origin, this._raycaster.ray.direction, 0xff0000, 10,);
-                
                 const opts = new RaycastOptions();
                 const hits = this.context.physics.raycastFromRay(ray, opts);
                 for (let i = 0; i < hits.length; i++) {
                     const hit = hits[i];
-                    //console.log(hit.object.name)
                     if(hit.object.name == "GroundPlane")
                     {
                         setWorldPosition(this.gameObject, hit.point);
                     }
                 }
-            }
-            
-        }
-        
-            
+            }   
+        }   
         }
     }
 }
